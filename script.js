13ad898e6a59a7dc0234220eb091838892f18a60
xapatheia <54541033+XAPATHEIA@users.noreply.github.com>
2023-09-30 17:46:43 +0100

    add basic grid visualization

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 let gridContainer = document.getElementById("grid-container");
 console.log(gridContainer)
+
+for(i = 0; i < 16; i++) {
+    newDiv = document.createElement("div");
+    newDiv.classList.add("gridElement");
+    gridContainer.appendChild(newDiv);
+}
