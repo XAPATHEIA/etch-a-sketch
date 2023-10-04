a51c45619502a3d46e4cc2414b7b28510f467aea
xapatheia <54541033+XAPATHEIA@users.noreply.github.com>
2023-10-04 20:15:06 +0100

    dynamically changing div sizes on the board depending on slider value

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let gridContainer = document.getElementById("grid-container");
 let slider = document.getElementById('slider'); // assigning the slider to a variable to retrieve its value
-let defaultValue = 16; // default size of the board
+let defaultNumberGrids = 16; // default size of the board
+let defaultBoardSize = 576;
 
 
 // function will draw the board depending on number of slider
 function drawBoard(numberOfGrids) {
-    for(i = 0; i < numberOfGrids; i++) {
+    for(i = 0; i < Math.pow(numberOfGrids, 2); i++) {
         newDiv = document.createElement("div");
         newDiv.classList.add("gridElement");
         gridContainer.appendChild(newDiv);
@@ -13,7 +14,19 @@ function drawBoard(numberOfGrids) {
 }
 
 
-drawBoard(defaultValue);
+drawBoard(defaultNumberGrids);
+
+
+function changeGridSizes(numberOfGrids) {
+    const grids = document.querySelectorAll(".gridElement");
+    grids.forEach((grid) => {
+        grid.style.width = Math.floor(defaultBoardSize / numberOfGrids) + "px";
+        grid.style.height = Math.floor(defaultBoardSize / numberOfGrids) + "px";
+        console.log((defaultBoardSize / numberOfGrids) + "px");
+        console.log(slider.value)
+    });
+}
+
 
 // listens to the slider change and updates board size
 slider.addEventListener('input', function() {
@@ -21,6 +34,7 @@ slider.addEventListener('input', function() {
         gridContainer.removeChild(gridContainer.lastChild);
     }
     drawBoard(slider.value);
+    changeGridSizes(slider.value);
 } 
 )
 
